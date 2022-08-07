92a5972c152aa421a28fb768dbb83df972454956
Aleksandra Angelova <aleksandra.angelova2@gmail.com>
2022-08-07 14:59:23 +0300

    Add trade details to Trade model - migration

--- models/trade.py
+++ models/trade.py
@@ -9,4 +9,12 @@ class Trade(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     created_on = db.Column(db.DateTime, nullable=False, server_default=func.now())
-    status = db.Column(db.Enum(TradeStatus), default=TradeStatus.pending, nullable=False)
+    requester_id = db.Column(db.Integer(), db.ForeignKey("trader.id"), nullable=False)
+    requester_cards = db.Column(db.ARRAY(db.Integer), nullable=False)
+    counterparty_id = db.Column(
+        db.Integer(), db.ForeignKey("trader.id"), nullable=False
+    )
+    counterparty_cards = db.Column(db.ARRAY(db.Integer), nullable=False)
+    status = db.Column(
+        db.Enum(TradeStatus), default=TradeStatus.pending, nullable=False
+    )
