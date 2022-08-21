10993abc30836bc5e6d45d9e4170054c893ddafa
Aleksandra Angelova <aleksandra.angelova2@gmail.com>
2022-08-21 12:49:08 +0300

    Finish trading cards - transfer from requestor to counterparty and vice versa

--- managers/trade.py
+++ managers/trade.py
@@ -1,12 +1,11 @@
 from db import db
-from models.trade import Trade
 from models.card import Card
 from models.enum import TradeStatus
+from models.trade import Trade
 from utils.common import get_trade
 
 
 class TradeManager:
-
     @staticmethod
     def create(data, user):
         data["requester_id"] = user.id
@@ -21,14 +20,20 @@ class TradeManager:
     @staticmethod
     def change_traded_cards_ownership(trade_id):
         trade = get_trade(trade_id)
+
+        # transfer cards from counterparty to requester
         cards_for_requester, requester_id = trade.counterparty_cards, trade.requester_id
         for card in cards_for_requester:
-            Card.query.filter_by(id=card).update(dict(owner_id=requester_id, tradeable=False))
+            Card.query.filter_by(id=card).update(
+                dict(owner_id=requester_id, tradeable=False)
+            )
 
-
-
-
-
-
-
-
+        # transfer cards from requester to counterparty
+        cards_for_counterparty, counterparty_id = (
+            trade.requester_cards,
+            trade.counterparty_id,
+        )
+        for card in cards_for_counterparty:
+            Card.query.filter_by(id=card).update(
+                dict(owner_id=counterparty_id, tradeable=False)
+            )
