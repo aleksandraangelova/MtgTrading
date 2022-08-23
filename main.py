3b0813bbaae8a11b30f0e77fb0776d34b2eb0003
Aleksandra Angelova <aleksandra.angelova2@gmail.com>
2022-08-23 19:23:17 +0300

    Add Swagger UI

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from config import create_app
 from db import db
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = create_app()
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yaml'
+
+swagger_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+)
+
+app.register_blueprint(swagger_blueprint)
 
 
 @app.after_request
